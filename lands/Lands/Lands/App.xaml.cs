29d545c6d276b87a4e5cf5931e2ce0fd39ff39a3
William.AparicioSM <William.AparicioSM@CDS1UNIVO25>
2018-10-25 11:02:33 -0600

    cambios en la interfaz

--- lands/Lands/Lands/App.xaml.cs
+++ lands/Lands/Lands/App.xaml.cs
@@ -15,7 +15,7 @@ namespace Lands
         {
             InitializeComponent();
 
-            MainPage = new LoginPage();
+            MainPage =new NavigationPage (new LoginPage());
         }
 
         #endregion
---
+++ lands/Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace Lands.Infrastructure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Lands.Infrastructure;
+    using ViewModel;
+
+    //instancia el locator para encontrar la main viewmodel
+    public class InstanceLocator
+    {
+        #region Propiedades
+
+        public MainViewModel  main
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructor
+        public InstanceLocator()
+        {
+            this.main = new MainViewModel();
+        }
+        #endregion
+
+        #region MyRegion
+
+        #endregion
+    }
+
+
+
+}
