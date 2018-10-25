3b360a750119d7049d5836cab1b4c491d579bc24
William.AparicioSM <William.AparicioSM@CDS1UNIVO25>
2018-10-25 09:42:18 -0600

    creacion de carpetas

---
+++ lands/Lands/Lands/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Lands
+{
+    using Lands.View;
+    using System;
+    using Xamarin.Forms;
+
+
+    public partial class App : Application
+    {
+        #region Constructor
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginPage();
+        }
+
+        #endregion
+
+        #region Metodos
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        } 
+        #endregion
+    }
+}
