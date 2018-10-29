49bdadce3ebb975c7ff702e06a760c4aa8a114d4
William.AparicioSM <William.AparicioSM@CDS1UNIVO25>
2018-10-29 11:13:21 -0600

    login viewmodel termiando

--- lands/Lands/Lands/ViewModel/LoginViewModel.cs
+++ lands/Lands/Lands/ViewModel/LoginViewModel.cs
@@ -1,9 +1,23 @@
 ﻿namespace Lands.ViewModel
 {
+    using GalaSoft.MvvmLight.Command;
+    using System;
+    using System.ComponentModel;
     using System.Windows.Input;
+    using Xamarin.Forms;
 
-    public class LoginViewModel
+    public class LoginViewModel : BaseViewModel
     {
+        
+
+        #region Atributes
+       // private string email;
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+
+        #endregion
+
         #region propiedades
         public string Email
         {
@@ -12,18 +26,41 @@
         }
         public string Password
         {
-            get;
-            set;
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                SetValue(ref this.password, value);
+            }
         }
         public bool IsRunning
         {
-            get;
-            set;
+            get
+            {
+                return this.isRunning;
+            }
+            set
+            {
+                SetValue(ref this.isRunning, value);
+            }
         }
         public bool IsRemembered
         {
             get;
             set;
+        }
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.isEnabled;
+            }
+            set
+            {
+                SetValue(ref this.isEnabled, value);
+            }
         }
         #endregion
 
@@ -31,14 +68,64 @@
         public LoginViewModel()
         {
             this.IsRemembered = true;
+         this.IsEnabled = true ;
         }
         #endregion
 
         #region Commandos
         public ICommand LoginCommand
         {
-            get;
-            set;
+            get
+            {
+                return new RelayCommand(Login);
+            }
+
+        }
+
+
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error ",
+                    "You must enter an email ",
+                    "Accept ");
+                return;
+            }
+            
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error ",
+                    "You must enter an password ",
+                    "Accept ");
+                
+                return;
+            }
+            this.IsRunning = true;
+            this.IsEnabled = false;
+            if (this.Email != "w" || this.Password != "123")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                   "ok ",
+                   "email or password incorrent ",
+                   "Accept ");
+                this.Password = string.Empty;
+                return;
+            }
+            this.IsRunning = false;
+            this.IsEnabled = true;
+            await Application.Current.MainPage.DisplayAlert(
+                 "ok ",
+                 "fuckyea ",
+                 "Accept ");
+            return;
         }
 
         #endregion
